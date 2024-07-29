65e9b5d78708efe22d5458e54e5fcb34789621e9
dia <dhiaa.kahri@gmail.com>
2024-07-29 18:18:35 +0100

    adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,59 @@
-import { useState } from 'react';
 import toast from 'react-hot-toast';
 
 import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import MainLayout from './layouts/main-layout.tsx';
+import AuthPage from './pages/AuthPage.tsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="text-center">Tailwind active</div>
-      <button
-        onClick={() => {
-          toast.success('toast works', {
-            duration: 4000,
-            position: 'top-center',
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div className="text-center">Tailwind active</div>
+                  <button
+                    onClick={() => {
+                      toast.success('toast works', {
+                        duration: 4000,
+                        position: 'top-center',
 
-            // Styling
-            style: {},
-            className: '',
+                        // Styling
+                        style: {},
+                        className: '',
 
-            // Custom Icon
-            icon: '👏',
+                        // Custom Icon
+                        icon: '👏',
 
-            // Change colors of success/error/loading icon
-            iconTheme: {
-              primary: '#000',
-              secondary: '#fff',
-            },
+                        // Change colors of success/error/loading icon
+                        iconTheme: {
+                          primary: '#000',
+                          secondary: '#fff',
+                        },
 
-            // Aria
-            ariaProps: {
-              role: 'status',
-              'aria-live': 'polite',
-            },
-          });
-        }}
-      >
-        Show
-      </button>
+                        // Aria
+                        ariaProps: {
+                          role: 'status',
+                          'aria-live': 'polite',
+                        },
+                      });
+                    }}
+                  >
+                    Show
+                  </button>
+                </>
+              }
+            />
+            <Route path="auth" element={<AuthPage />} />
+            <Route path="*" element={<h1>Page Not Found</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/*  */}
     </>
   );
 }
